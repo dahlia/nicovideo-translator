6e594adeb8a45992a970ba8b024b8e7ec5eb223c
Hong Minhee <hongminhee@member.fsf.org>
2015-03-30 00:51:24 +0900

    Manually resolve msg.nicovideo.jp
    We resolve the hostname of msg.nicovideo.jp using dns package,
    to workaround the situation that msg.nicovideo.jp refers to 127.0.0.1
    in /etc/hosts file.

--- lib/NicovideoTranslator/Proxy.hs
+++ lib/NicovideoTranslator/Proxy.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
-module NicovideoTranslator.Proxy (app) where
+module NicovideoTranslator.Proxy
+    ( ProxyConfiguration(ProxyConfiguration)
+    , app
+    , language
+    , upstreamHost
+    ) where
 
 import Data.List (find)
 import Data.Maybe (catMaybes)
@@ -11,11 +16,10 @@ import Data.CaseInsensitive (CI)
 import Data.LanguageCodes (ISO639_1)
 import Data.Set (Set, fromList, notMember)
 import Data.Text (Text, unpack)
-import Data.Text.Encoding (decodeUtf8)
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Network.HTTP.Types.Method (Method)
 import Network.Wai (Application, rawPathInfo, rawQueryString,
-                    responseLBS, requestBody, requestHeaders, requestMethod,
-                    requestHeaderHost)
+                    responseLBS, requestBody, requestHeaders, requestMethod)
 import Network.Wreq (Options, checkStatus, defaults, deleteWith, getWith,
                      headers, postWith, putWith, responseBody,
                      responseHeader, responseHeaders, responseStatus)
@@ -27,21 +31,20 @@ import Text.XML.Cursor (content, element, fromDocument, node, ($//), (&//))
 
 import NicovideoTranslator.Translate (translateMultiple)
 
-app :: ISO639_1 -> Application
-app language req respond =
+data ProxyConfiguration = ProxyConfiguration { language :: ISO639_1
+                                             , upstreamHost :: Text
+                                             }
+
+app :: ProxyConfiguration -> Application
+app config req respond =
     let path = rawPathInfo req
         qs = rawQueryString req
-        host = case requestHeaderHost req of
-                   Just hostname -> hostname
-                   Nothing -> defaultHost
+        host = encodeUtf8 $ upstreamHost config
         pathQuery = B.append path qs
         hostUrl = B.append "http://" host
         url = B.append hostUrl pathQuery
         urlString = decodeUtf8 url
-    in proxyApp language urlString req respond
-
-defaultHost :: B.ByteString
-defaultHost = "msg.nicovideo.jp"
+    in proxyApp config urlString req respond
 
 hoppishHeaders :: Set (CI B.ByteString)
 hoppishHeaders = fromList [ "connection"
@@ -56,8 +59,8 @@ hoppishHeaders = fromList [ "connection"
                           , "content-encoding"
                           ]
 
-proxyApp :: ISO639_1 -> Text -> Application
-proxyApp language url req respond = do
+proxyApp :: ProxyConfiguration -> Text -> Application
+proxyApp config url req respond = do
     body <- requestBody req
     response <- request method options (unpack url) body
     let rBody = response ^. responseBody
@@ -67,7 +70,7 @@ proxyApp language url req respond = do
         rHeaders = (response ^. responseHeaders)
         toBeTranslated = method == "POST" && mimetype == "text/xml"
     translated <- if toBeTranslated
-                  then translateResponse language rBody
+                  then translateResponse (language config) rBody
                   else return rBody
     respond $ responseLBS rStatus rHeaders translated
   where
@@ -87,14 +90,14 @@ request "DELETE" = \options url _ -> deleteWith options url
 request _= \_ _ _ -> ioError $ userError $ "unsupported method"
 
 translateResponse :: ISO639_1 -> LB.ByteString -> IO LB.ByteString
-translateResponse language response = case parseLBS def response of
+translateResponse lang response = case parseLBS def response of
     Left _ -> return response
-    Right doc -> do translatedDoc <- translateXml language doc
+    Right doc -> do translatedDoc <- translateXml lang doc
                     return $ renderLBS def translatedDoc
 
 translateXml :: ISO639_1 -> Document -> IO Document
-translateXml language doc = do
-    translatedTexts <- translateMultiple language texts
+translateXml lang doc = do
+    translatedTexts <- translateMultiple lang texts
     let translatedElems = [ (el, el { elementNodes = [NodeContent text] })
                           | (el, text) <- zip elems translatedTexts ]
     return $ transformXml doc translatedElems
